8aab822078efc941d70ffe785912c50b669ccb1a
Nai64 <k3gn@proton.me>
2026-06-09 19:15:11 +0500

    Release 3.3.0

--- bandcamp_auto_uploader/__version__.py
+++ bandcamp_auto_uploader/__version__.py
@@ -15,8 +15,8 @@ from __future__ import annotations
 
 import re
 
-__version__ = "3.2.1"
-__version_label__ = "3.2.1"
+__version__ = "3.3.0"
+__version_label__ = "3.3.0"
 
 _VERSION_RE = re.compile(r"^\d+(?:\.\d+)*(?:(?:[ab]|rc)\d*)?$")
 
